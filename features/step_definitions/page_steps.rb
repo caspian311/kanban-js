7b5b53f7d928234c97b208b1f66a8de9234959c3
matt.todd <matt.todd@asolutions.com>
2012-01-12 06:44:54 -0600

    enhanced my first cuke to exercise the story management

--- features/step_definitions/page_steps.rb
+++ features/step_definitions/page_steps.rb
@@ -2,17 +2,35 @@ When /^I go to the "([^"]*)" page$/  do |page|
    visit path_to(page)
 end
 
+When /^I follow "([^"]*)"$/ do |text|
+   click_link text
+end
+
+When /^I don't fill in anything$/ do
+end
+
+When /^I fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+   fill_in field, :with => value
+end
+
+When /^I press "([^"]*)"$/ do |button|
+   click_button button
+end
+
 Then /^I should see "([^"]*)"$/ do |text|
+   if text == "TODAY"
+      text = DateTime.now.strftime "%m/%d/%Y"
+   end
+
    page.should have_content(text)
 end
 
-And /^I don't fill in anything$/ do
+Then /^I should not see "([^"]*)"$/ do |text|
+   page.should_not have_content(text)
 end
 
-And /^I fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
-   fill_in(field, :with => value)
+Then /^I should be on the "([^"]*)" page$/ do |page|
+   uri = URI.parse(current_url)
+   uri.path.should == path_to(page)
 end
 
-And /^I press "([^"]*)"$/ do |button|
-   click_button(button)
-end
--- features/stories.feature
+++ features/stories.feature
@@ -4,4 +4,19 @@ Feature: Stories
    Scenario: User can view, add, edit, and remove user stories
       When I go to the "stories" page
       Then I should see "Stories"
+         And I should see "No stories have been entered yet"
+      When I follow "Enter a new story"
+      Then I should see "Create"
+      When I fill in "name" with "story name"
+         And I fill in "description" with "this is the story description"
+         And I press "Create new story"
+      Then I should be on the "stories" page
+         And I should see "story name"
+         And I should see "this is the story description"
+         And I should see "TODAY"
+      When I follow "Remove story"
+      Then I should be on the "stories" page
+         And I should not see "story name"
+         And I should not see "this is the story description"
+         And I should see "No stories have been entered yet"
 
