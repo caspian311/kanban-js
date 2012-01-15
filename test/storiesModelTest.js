63fadba5e9097cba7fe5a1e34807295ca45a1718
matt.todd <matt.todd@asolutions.com>
2012-01-15 06:43:27 -0600

    refactored the data module to return back the class not an object

--- test/storiesModelTest.js
+++ test/storiesModelTest.js
@@ -1,14 +1,16 @@
 var should = require('should');
 var util = require('util');
 
-var StoriesModel = require('../lib/storiesModel');
-var testObject = new StoriesModel();
-
 var Data = require('../lib/data');
+
+var _data = new Data();
+
+var StoriesModel = require('../lib/storiesModel');
+var testObject = new StoriesModel(_data);
 
 describe('StoriesModel', function() {
    beforeEach(function(done) {
-      Data.Story.find(function(err, docs){
+      _data.Story.find(function(err, docs){
          docs.forEach(function(doc) {
             doc.remove();
          });
@@ -18,11 +20,11 @@ describe('StoriesModel', function() {
 
    describe('#listAll', function() {
       it('should return all stories', function(done) {
-         var story1 = new Data.Story();
+         var story1 = new _data.Story();
          story1.name = 'test1';
          story1.save();
          
-         var story2 = new Data.Story();
+         var story2 = new _data.Story();
          story2.name = 'test2';
          story2.save();
          
