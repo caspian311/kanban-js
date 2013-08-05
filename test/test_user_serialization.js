b1f2daa1930943928c9574c7182d9f4961fc3514
Matt Todd <matt.c.todd@gmail.com>
2013-08-04 21:25:08 -0500

    more tests for authentication things [#5]

---
+++ test/test_user_serialization.js
@@ -0,0 +1,15 @@
+(function() {
+   var userSerialization = require('../app/authentication/user_serialization');
+
+   describe('UserSerialization', function() {
+      describe('#serialize', function() {
+         it('should ...', function() {
+         });
+      });
+
+      describe('#deserialize', function() {
+         it('should ...', function() {
+         });
+      });
+   });
+})();
