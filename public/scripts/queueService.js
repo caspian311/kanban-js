d2c13542a3302dc8dd169e89092bbd1c8dc0c1a2
Matt Todd <matt.c.todd@gmail.com>
2013-08-12 13:10:44 -0500

    extracting out queue data to service [#13]

--- public/scripts/queueService.js
+++ public/scripts/queueService.js
@@ -1,3 +1,12 @@
 define(function() {
-   return {};
+   var QueueService = function() {
+      this.getAllQueues = function() {
+         return [
+            { name: 'task 1', description: 'desc 1'},
+            { name: 'task 2', description: 'desc 2'},
+            { name: 'task 3', description: 'desc 3'},
+            { name: 'task 4', description: 'desc 4'}]
+      };
+   };
+   return new QueueService();
 });
--- public/scripts/viewModels/queueManagement.js
+++ public/scripts/viewModels/queueManagement.js
@@ -1,12 +1,8 @@
 define(['queueService'], function(queueService) {
    var QueueManagement = function() {
-      this.queues = ko.observableArray([
-         { name: 'task 1', description: 'desc 1'},
-         { name: 'task 2', description: 'desc 2'},
-         { name: 'task 3', description: 'desc 3'},
-         { name: 'task 4', description: 'desc 4'}
-      ]);
-      this.foo = 'abc';
+      this.queues = ko.observableArray();
+
+      this.queues(queueService.getAllQueues());
    };
    return new QueueManagement();
 });
