fc95f1bcccf5d08774476b418f10a9c7d8e0e78a
Matt Todd <matt.c.todd@gmail.com>
2013-08-18 15:43:46 -0500

    used different db based on environment vairable [#20]

--- app/db/dbHelper.js
+++ app/db/dbHelper.js
@@ -1,7 +1,15 @@
 (function() {
    var DbHelper = function() {
+      var db = function() {
+         if (process.env.NODE_ENV === 'test') {
+            return 'test';
+         } else {
+            return 'kanbanjs';
+         }
+      };
+
       this.getConnectionString = function() {
-         return 'mongodb://localhost:27017/test'
+         return 'mongodb://localhost:27017/' + db();
       };
    };
 
