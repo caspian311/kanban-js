e218d8660c240d92399918e590148305cea11f79
Matt Todd <matt.c.todd@gmail.com>
2013-08-22 15:25:22 -0500

    added queue service call to get queues and tabs for the different queues [#23]

--- public/scripts/viewModels/home.js
+++ public/scripts/viewModels/home.js
@@ -1,4 +1,4 @@
-define(function() {
+define(['services/queueService'], function(queueService) {
    var Card = function(name) {
       var description = "blah blah blah blah blah blah blah blah blah blah blah";
       this.name = ko.observable(name);
@@ -10,18 +10,49 @@ define(function() {
       this.cards = ko.observableArray(cards);
    }
 
-   var ViewModel = function() {
-      var allStates = [
-         new State('To do', 
-            [new Card('card1'), new Card('card2'), new Card('card3'), new Card('card4')]),
-         new State('Doing', 
-            [new Card('card5'), new Card('card6')]),
-         new State('Done', 
-            [new Card('card7'), new Card('card8'), new Card('card9'), new Card('card10')])
-      ];
+   var allStates = [
+      new State('To do', 
+         [new Card('card1'), new Card('card2'), new Card('card3'), new Card('card4')]),
+      new State('Doing', 
+         [new Card('card5'), new Card('card6')]),
+      new State('Done', 
+         [new Card('card7'), new Card('card8'), new Card('card9'), new Card('card10')])
+   ];
 
+   var Queue = function() {
       this.states = ko.observableArray(allStates);
    };
 
-   return new ViewModel();
+   var Home = function() {
+      var self = this;
+      self.queues = ko.observableArray([]);
+      self.selectedQueue = ko.observable();
+
+
+      self.viewAttached = function() {
+         queueService.getAllQueues(function(queues) {
+            self.queues(transformQueues(queues));
+            if (self.queues().length > 0) {
+               self.selectedQueue(self.queues()[0]);
+            }
+         });
+      };
+
+      var transformQueues = function(queuesJson) {
+         return [];
+      };
+
+      self.selectQueue = function(queue) {
+         self.selectedQueue(queue);
+      };
+
+      self.states = ko.computed(function() {
+         if (self.selectedQueue()) {
+            return self.selectedQueue().states();
+         }
+         return [];
+      });
+   };
+
+   return new Home();
 });
