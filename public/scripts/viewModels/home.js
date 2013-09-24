86abe395353e7f1bb5ff97c34ead15b62a0b43a7
Matt Todd <matt.c.todd@gmail.com>
2013-09-24 09:04:28 -0500

    dont show new card button if there are no queues [#78]

--- public/scripts/viewModels/home.js
+++ public/scripts/viewModels/home.js
@@ -89,6 +89,9 @@ define(['services/queueService', 'navigation', 'growler'], function(queueService
       var self = this;
       self.queues = ko.observableArray([]);
       self.selectedQueue = ko.observable();
+      self.canCreateCard = ko.computed(function() {
+         return self.queues().length > 0;
+      });
 
       self.viewAttached = function() {
          queueService.getAllQueues(populateQueues);
