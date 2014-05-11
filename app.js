e309a68350374826cc88ce97e5b5518974d9f3d8
Matt Todd <matt.c.todd@gmail.com>
2014-05-10 20:03:36 -0500

    removed module pattern

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
-(function() {
-  var express = require('express')
-     , http = require('http')
-     , path = require('path')
-     , main = require('./app/main')
-     , queues = require('./app/queues')
-     , cards = require('./app/cards')
-     , login = require('./app/login')
-     , logout = require('./app/logout')
-     , registration = require('./app/registration')
-     , authentication = require('./app/authentication');
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , main = require('./app/main')
+  , queues = require('./app/queues')
+  , cards = require('./app/cards')
+  , login = require('./app/login')
+  , logout = require('./app/logout')
+  , registration = require('./app/registration')
+  , authentication = require('./app/authentication');
 
-   var app = express()
+var app = express()
 
-   app.use(express.logger('dev'))
-   app.use(express.static(path.join(__dirname, 'public')))
-   app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'jade')
 
-   app.use(express.bodyParser())
-   app.use(express.methodOverride())
-   app.use(express.favicon())
-   app.use(express.cookieParser())
-   app.use(express.session({ secret: 'keyboard cat' }))
+app.use(express.bodyParser())
+app.use(express.methodOverride())
+app.use(express.favicon())
+app.use(express.cookieParser())
+app.use(express.session({ secret: 'keyboard cat' }))
 
-   app.use(authentication)
+app.use(authentication)
 
-   app.use(main)
-   app.use(queues)
-   app.use(cards)
-   app.use(login)
-   app.use(logout)
-   app.use(registration)
+app.use(main)
+app.use(queues)
+app.use(cards)
+app.use(login)
+app.use(logout)
+app.use(registration)
 
-   http.createServer(app).listen(3000, function(){
-     console.log("Express server listening on port 3000")
-   });
-})();
+http.createServer(app).listen(3000, function(){
+  console.log("Express server listening on port 3000")
+});
